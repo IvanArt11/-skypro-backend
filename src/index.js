4ce2dfe0e6589a517a531ead93a3f622a9fa305f
IvanArt11 <artemiev1990@mail.ru>
2023-12-31 00:38:16 +0300

    Corrected hw-2

--- src/index.js
+++ src/index.js
@@ -1,55 +1,64 @@
 const http = require("http");
 const getUsers = require("./modules/users");
-const hostname = "http://127.0.0.1";
+const hostname = "127.0.0.1";
 const port = 3003;
 
 const server = http.createServer((request, response) => {
-  const url = new URL(request.url, hostname);
-  const params = url.searchParams;
+  // Парсим URL запроса
+  const url = new URL(request.url, `http://${hostname}:${port}`);
 
-  // если параметры переданы...
-  if (params.toString() !== "") {
-    // строка поиска не пуста
-    if (params.has("hello")) {
-      //если в запросе параметр: "hello" (вернет Boolean)
-      const name = params.get("hello"); // возвращаем первое значение, связанное с заданным параметром поиска
+  // Если запрос не к корневому эндпоинту, возвращаем 500
+  if (url.pathname !== "/") {
+    response.statusCode = 500;
+    response.setHeader("Content-Type", "text/plain");
+    response.write("");
+    response.end();
+    return;
+  }
+
+  // Проверяем наличие параметров запроса
+  if (url.searchParams.toString() !== "") {
+    if (url.searchParams.has("hello")) {
+      // Если есть параметр hello, формируем соответствующий ответ
+      const name = url.searchParams.get("hello");
       if (name) {
-        response.status = 200;
-        response.header = "Content-Type: text/plain";
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/plain");
         response.write(`Hello, ${name}.`);
         response.end();
         return;
       } else {
-        response.status = 400;
-        response.header = "Content-Type: text/plain";
+        // Если параметр hello указан, но отсутствует значение
+        response.statusCode = 400;
+        response.setHeader("Content-Type", "text/plain");
         response.write("Enter a name");
         response.end();
         return;
       }
-    } else if (params.has("users")) {
-      response.status = 200;
-      response.statusMessage = "OK";
-      response.header = "Content-Type: application/json";
-      response.write(getUsers());
+    } else if (url.searchParams.has("users")) {
+      // Если есть параметр users, возвращаем данные пользователей
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "application/json");
+      response.write(JSON.stringify(getUsers()));
       response.end();
       return;
     } else {
-      response.status = 500;
-      response.header = "Content-Type: text/plain";
+      // Если переданы другие параметры запроса
+      response.statusCode = 500;
+      response.setHeader("Content-Type", "text/plain");
       response.write("");
       response.end();
       return;
     }
   }
 
-  // Если никакие параметры не переданы...
-  response.status = 200;
-  response.statusMessage = "OK";
-  response.header = "Content-Type: text/plain";
+  // Если параметры не переданы, возвращаем приветственное сообщение
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/plain");
   response.write("Hello, world!");
   response.end();
 });
 
-server.listen(port, () => {
-  console.log(`Сервер запущен по адресу ${hostname}:${port}`);
+server.listen(port, hostname, () => {
+  console.log(`Сервер запущен по адресу http://${hostname}:${port}`);
 });
